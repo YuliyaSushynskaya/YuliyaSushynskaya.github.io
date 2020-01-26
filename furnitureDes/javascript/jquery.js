86c13db09886918ed8bc04757949a530f37908a9
YuliyaSushynskaya <60312963+YuliyaSushynskaya@users.noreply.github.com>
2020-01-26 18:49:47 +0300

    vds

--- furnitureDes/javascript/jquery.js
+++
@@ -1,7 +0,0 @@
-﻿$(window).on('load', function () {
-    setTimeout(function() {
-		$loader = $('body').find('#preloader');
-		$loader.fadeOut();
-		$loader.delay(350).fadeOut('slow');
-	}, 4000);//секунды
- });
--- furnitureDes/javascript/media.js
+++
@@ -1,8 +0,0 @@
-﻿function playAudio()
-{
-	var imgAudio=document.getElementById("audioButtom");
-	var myaudio = document.getElementById("audioVolu");
-	myaudio.paused ? myaudio.play() : myaudio.pause();
-	imgAudio.className = (imgAudio.className == 'square' ? 'square-new' : 'square')
-}
-

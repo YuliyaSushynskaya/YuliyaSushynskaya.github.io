04cb921ca73a402eeb9215b0646a9354a99c3894
YuliyaSushynskaya <60312963+YuliyaSushynskaya@users.noreply.github.com>
2020-01-26 19:21:45 +0300

    furnitureDes
    html5/css3/js/jQuery

---
+++ furnitureDes/ИТОГ_Iframe/javascript/scriptJquery.js
@@ -0,0 +1,14 @@
+﻿window.onload = function() { 
+	setInterval(function() { 
+		right();
+	},5000);
+};
+
+slides=['searh1.jpg','searh2.jpg','searh3.jpg','searh4.jpg'],
+frame=0; 
+var i = slides.length; 
+$( right=function () { 
+	frame--;
+	if(frame < 0) frame = i-1;
+		$("#scr").css('backgroundImage','url(img/'+slides[frame]+')');
+});
